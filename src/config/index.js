9b0a854a0196bd6683570b1253d82890981dc9ac
IngRodro <rr0213032019@unab.edu.sv>
2022-10-16 22:10:48 -0600

    Test

--- src/config/index.js
+++ src/config/index.js
@@ -1,26 +1,16 @@
-const {
-  APP_PORT,
-  APP_DATABASE_URL,
-  APP_DATABASE_URL_TEST,
-  APP_CLOUD_NAME,
-  APP_API_KEY,
-  APP_API_SECRET,
-  NODE_ENV,
-  TOKEN_SECRET,
-} = process.env;
 const config = () => ({
-  port: APP_PORT || 8080,
+  port: process.env.APP_PORT || 8080,
   database: {
-    uri: NODE_ENV === 'test' ? APP_DATABASE_URL_TEST : APP_DATABASE_URL,
+    uri: process.env.NODE_ENV === 'test' ? process.env.APP_DATABASE_URL_TEST : process.env.APP_DATABASE_URL,
     options: {},
   },
   imageCloud: {
-    cloud_name: APP_CLOUD_NAME,
-    api_key: APP_API_KEY,
-    api_secret: APP_API_SECRET,
+    cloud_name: process.env.APP_CLOUD_NAME,
+    api_key: process.env.APP_API_KEY,
+    api_secret: process.env.APP_API_SECRET,
   },
   Token: {
-    secret: TOKEN_SECRET,
+    secret: process.env.TOKEN_SECRET,
   },
 });
 
