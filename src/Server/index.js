342e2e89e6df0db034a88ae1d619485a9a3bc760
IngRodro <rr0213032019@unab.edu.sv>
2022-10-27 15:20:31 -0600

    Token

--- src/Server/index.js
+++ src/Server/index.js
@@ -2,6 +2,12 @@ import express from 'express';
 import cors from 'cors';
 import { initializeDB } from './db';
 
+const corsOptions = {
+  origin: 'http://localhost:3000',
+  optionsSuccessStatus: 200,
+  headers: 'Content-Type, auth-token',
+};
+
 export const app = express();
 
 // creating Server
@@ -9,7 +15,7 @@ export const initializeServer = async (routes) => {
   // initialize DB
   await initializeDB();
 
-  app.use(cors());
+  app.use(cors(corsOptions));
 
   // json parse
   app.use(express.json());
